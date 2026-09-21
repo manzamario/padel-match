a6b847015f8e30229f71cde4fc4b895a9817a289
Mario Manzanelli <manzanellimario@gmail.com>
2026-09-21 16:52:23 -0300

    fix: Invitation schema must define _id as String (was ObjectId default)

--- models/Invitation.js
+++ models/Invitation.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 
 const invitationSchema = new mongoose.Schema({
+  _id: { type: String },
   fromPlayer: { type: String, ref: 'Player', required: true },
   toPlayer: { type: String, ref: 'Player', required: true },
   date: { type: String, default: '' },
